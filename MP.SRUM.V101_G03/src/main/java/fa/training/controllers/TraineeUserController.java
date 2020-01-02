6a41ea03e458dc53bdbc3aed8283c13f96dc08f5
LeeHoangT <leevhoang95@gmail.com>
2020-01-02 17:04:16 +0700

    update 02/01

--- MP.SRUM.V101_G03/src/main/java/fa/training/controllers/TraineeUserController.java
+++ MP.SRUM.V101_G03/src/main/java/fa/training/controllers/TraineeUserController.java
@@ -1,5 +1,20 @@
 package fa.training.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author HoangLV7
+ *
+ */
+@Controller
+@RequestMapping("trainee")
 public class TraineeUserController {
 
+	@GetMapping("/")
+	  public String index() {
+	    return "trainee-ui";
+	  }
+	
 }
