27f27ac00e121a1266023297b055e7b1d4ceea44
TrangDM <agent09@TrangDMs-MacBook-Pro.local>
2020-01-20 00:28:19 +0700

    edit html file

--- MP.SRUM.V101_G03/src/test/java/fa/training/adminservicetest/AdminClassServiceTest.java
+++ MP.SRUM.V101_G03/src/test/java/fa/training/adminservicetest/AdminClassServiceTest.java
@@ -1,36 +1,50 @@
 package fa.training.adminservicetest;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.*;
 
-import java.time.LocalDate;
-
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import fa.training.models.Clazz;
 import fa.training.repositories.ClassRepository;
+import fa.training.repositories.SubjectRepository;
+import fa.training.repositories.UserRepository;
 import fa.training.services.implement.AdminClassServiceImpl;
 
 /**
  * @author TrangDM2
  *
  */
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class AdminClassServiceTest {
 
   @Mock
   private ClassRepository classRepository;
 
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private SubjectRepository subjectRepository;
+  
   @InjectMocks
   private AdminClassServiceImpl adminClassService;
 
   @Before
-  public static void setUp() throws Exception {
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @After
+  public void tearDown() throws Exception {
   }
 
   /**
@@ -38,20 +52,25 @@ public class AdminClassServiceTest {
    */
   @Test
   public void testSaveClassSuccess() {
-    Clazz clazz = new Clazz(null, "JAVA", LocalDate.now(), "new class", "java", "Active");
+    Clazz clazz = new Clazz();
     when(adminClassService.saveClass(clazz, clazz.getStatus())).thenReturn(new Clazz());
-    assertEquals(clazz, adminClassService.saveClass(clazz, clazz.getStatus()));
+    assertNotNull(adminClassService.saveClass(clazz, clazz.getStatus()));
   }
 
   /**
    * @author TrangDM2
    */
-  @Test(expected = IllegalArgumentException.class)
+  @Test(expected = NullPointerException.class)
   public void testSaveClassFail() {
     Clazz clazz = new Clazz();
-//    doThrow(new IllegalArgumentException()).when(classRepository).save(clazz);
-    when(adminClassService.saveClass(clazz, clazz.getStatus())).thenReturn(null);
-    assertEquals(clazz, adminClassService.saveClass(clazz, clazz.getStatus()));
+    when(adminClassService.saveClass(clazz, clazz.getStatus())).thenReturn(new Clazz());
+    assertNotNull(adminClassService.saveClass(null, null));
+  }
+  
+  @Test
+  public void testAddUserToClassSuccess() {
+    when(adminClassService.addSubjectToClass(anyInt(), anyInt())).thenReturn(new Clazz());
+    assertNotNull(adminClassService.addUserToClass(anyInt(), anyInt()));
   }
 
 }
